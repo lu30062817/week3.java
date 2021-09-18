454baf86306462b84373fa2de3e22ea855985ce1
lu30062817 <90741645+lu30062817@users.noreply.github.com>
2021-09-18 01:55:25 -0500

    Create fishtank.java

---
+++ fishtank.java
@@ -0,0 +1,15 @@
+public class FishTank {
+    public void categories()
+    {
+    	System.out.println("Categories: Small, mid-sized and Large");
+    }
+    
+    public void types() {
+    	System.out.println("Types: ColdWaterTank, Tropical Freshwater tank, Reef tank, Brackish tank etc..");
+    }
+    
+    public void capacity() {
+    	System.out.println("Filled capacity varies from 5 gallon to 180 gallons");
+    }
+}
+
